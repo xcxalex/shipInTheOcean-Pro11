5e8da8dcd228ca2bede2a0283715497f986ad9e3
xcxalex <127541318+xcxalex@users.noreply.github.com>
2023-04-17 18:11:03 -0400

    Add files via upload

---
+++ PROC11_Plantilla_Proyecto_V4-main/sketch.js
@@ -0,0 +1,34 @@
+
+var ship;
+var sea;
+var shipImg;
+var seaImg
+function preload(){
+shipImg = loadAnimation("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
+  seaImg = loadImage("sea.png");
+  
+  }
+
+function setup(){
+  sea = createSprite (200,200,200,200);
+  sea.addImage("bground",seaImg);
+  sea.scale=1;
+  sea.velocityX=-2;
+  
+
+  ship = createSprite (150,300,200,200);
+  ship.addAnimation ("movingShip",shipImg);
+  ship.scale=0.25;
+}
+  
+if (sea.x < 0){
+  sea.x = sea.width/2;
+}
+function draw() {
+ 
+  background(220);
+  drawSprites();
+   
+  
+
+}
